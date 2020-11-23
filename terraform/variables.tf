af143e00e9d949fbdf162ad9ad42e3ad2adf1c90
Rory Preddy <ropreddy@microsoft.com>
2020-11-23 12:13:36 +0200

    Revert "new sku names"
    This reverts commit d9b1dad38d7f028b2473e46fd36ef8c229369db9.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "spring-petclinicrpza"
+  default = "spring-petclinic"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "spring-petclinic-apprpza"
+  default     = "spring-petclinic-app"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westus2"
+  default     = "westeurope"
 }
 
 variable "my_sql_master_password" {
