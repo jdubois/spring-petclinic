d9b1dad38d7f028b2473e46fd36ef8c229369db9
Rory Preddy <ropreddy@microsoft.com>
2020-11-23 12:04:21 +0200

    new sku names

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,16 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "spring-petclinic"
+  default = "spring-petclinicrpza"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "spring-petclinic-app"
+  default     = "spring-petclinic-apprpza"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westeurope"
+  default     = "westus2"
 }
 
 variable "my_sql_master_password" {
